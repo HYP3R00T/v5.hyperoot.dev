dd221d4be96b78bd8143e9672febc2f55fca1cca
hyperoot <rajesh@hyperoot.dev>
2025-02-22 11:47:47 +0000

    chore: clean up

---
+++ config.ts
@@ -0,0 +1,13 @@
+export const SITE = {
+  website: "https://hyperoot.dev",
+  author: "Rajesh",
+  repo: "https://github.com/HYP3R00T/v4.hyperoot.dev",
+  title: "Hyperoot",
+  description:
+    "A portfolio and blog showcasing Rajesh's skills in DevOps, Python, and Cloud.",
+  image: "/og-image.jpg",
+  imageAlt: "Check out hyperoot.dev",
+  contentType: "Portfolio",
+  twitterHandle: "@HYP3R00T",
+  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+};
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,25 @@
 import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
+import { SITE } from 'config';
 
-const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+const article = defineCollection({
+  // Load Markdown and MDX files in the `src/content/blog/` directory.
+  loader: glob({ base: "./content/blog", pattern: "**/*.{md,mdx}" }),
+  // Type-check frontmatter using a schema
+  schema: ({ image }) =>
+    z.object({
+      author: z.string().default(SITE.author),
+      pubDatetime: z.date(),
+      modDatetime: z.date().optional().nullable(),
+      title: z.string(),
+      featured: z.boolean().optional().default(false),
+      draft: z.boolean().optional().default(false),
+      category: z.string(),
+      tags: z.array(z.string()).default([""]),
+      description: z.string(),
+      cover: image().optional(),
+      coverAlt: z.string().optional(),
+    }),
 });
 
-export const collections = { blog };
+export const collections = { article };
