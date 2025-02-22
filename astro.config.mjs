2696c153a395db39b52d1cd4857fbd7b76fcf581
hyperoot <rajesh@hyperoot.dev>
2025-02-22 12:11:01 +0000

    feat: add icons support using local svg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-
+  site: "https://example.com",
   vite: {
+    // @ts-ignore
     plugins: [tailwindcss()],
   },
+  integrations: [
+    mdx(),
+    sitemap(),
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+  ],
 });
