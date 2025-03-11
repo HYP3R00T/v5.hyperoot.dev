c6c461faa111e7a4d50d9f3073c7b285491dce75
hyperoot <rajesh@hyperoot.dev>
2025-03-11 19:46:16 +0000

    feat: plugins added

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,35 @@ import tailwindcss from "@tailwindcss/vite";
 
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+import AutoImport from "astro-auto-import";
+import rehypeExternalLinks from "rehype-external-links";
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hyperoot.dev",
   vite: {
     plugins: [tailwindcss()],
   },
-
+  markdown: {
+    shikiConfig: {
+      theme: "css-variables",
+      defaultColor: false,
+    },
+    rehypePlugins: [
+      [
+        rehypeExternalLinks,
+        {
+          target: "_blank",
+        },
+      ],
+    ],
+  },
   integrations: [
+    AutoImport({
+      imports: ["./src/components/core/Card.astro"],
+    }),
+    mdx(),
     icon({
       iconDir: "src/assets/icons",
     }),
